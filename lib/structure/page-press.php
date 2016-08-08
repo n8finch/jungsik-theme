a8de38b31b71b8e27728f01f970c3d35e93a820e
Nate Finch <finchproservices@gmail.com>
2016-08-08 19:59:53 +0900

    layout and styles for most of press page done

--- lib/structure/page-press.php
+++ lib/structure/page-press.php
@@ -8,7 +8,7 @@ add_action( 'genesis_before', __NAMESPACE__ . '\customize_press_page' );
  * Do all work within is_front_page conditional
  */
 function customize_press_page() {
-	if ( is_page('press') ) {
+	if ( is_page( 'press' ) ) {
 		remove_all_actions( 'genesis_loop' );
 		add_action( 'genesis_before_content_sidebar_wrap', __NAMESPACE__ . '\add_bar_page_contents' );
 	}
@@ -17,19 +17,17 @@ function customize_press_page() {
 function add_bar_page_contents() {
 
 	//Get the data
-	$press_items = get_field('press_items');
+	$press_items = get_field( 'press_items' );
 
 	echo '<div class="press-items">';
 
 	foreach ( $press_items as $row ) {
 		echo '<div class="press-item">' .
-		     '<img src="'.$row["press_item_image"].'"/>' .
+		     '<a href="'.$row['press_item_link'].'" target="_blank"><img class="press-item-pic" src="' . $row["press_item_image"] . '"/></a>' .
 		     '<br/>' .
-		     $row['press_item_title'] .
+		     '<a href="'.$row['press_item_link'].'" target="_blank"><h3>' . $row['press_item_title'] . '</h3></a>' .
 		     '<br/>' .
 		     $row['press_item_content'] .
-		     '<br/>' .
-		     $row['press_item_link'] .
 		     '<hr/>' .
 		     '</div>';
 	}
